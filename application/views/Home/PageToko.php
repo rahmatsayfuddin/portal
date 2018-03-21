4c9c75993b7a209f430d9cd55d6ac5f7564f85a3
Unknown <rahmat.sayfuddin@binus.ac.id>
2018-03-21 17:31:58 +0700

    iframe

---
+++ application/views/Home/PageToko.php
@@ -0,0 +1,4 @@
+<iframe src="https://10.80.170.19:8443/clmui/#/" style="width: 100%; height: 100%;border: none;"></iframe>
+<?php 
+// /echo file_get_contents('https://10.80.170.19:8443/clmui/#/');
+?>
